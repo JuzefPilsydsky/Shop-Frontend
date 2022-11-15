501627e3e0db8943317916eedeac97b7dc370bbf
JuzefPilsudsky <strateichukmaksym@gmail.com>
2022-11-15 21:45:15 +0200

    Project initialization

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <top-bar-main/>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/TopBarInMain.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <v-toolbar
+        dark
+        prominent
+        src="https://www.elegantthemes.com/blog/wp-content/uploads/2013/09/bg-9-full.jpg"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title class="tool-bar-title">Welcome to my first test online shop</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </div>
+  </template>
+
+  <script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
